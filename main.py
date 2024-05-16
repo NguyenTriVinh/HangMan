2b00b553b3fe629695e839d7d505e24e573607bf
Nguyen Tri Vinh <131792024+NguyenTriVinh@users.noreply.github.com>
2024-05-16 21:22:00 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+from hangman_words import word_list
+choosen_word = random.choice(word_list)
+word_length = len(choosen_word)
+
+end_game = True
+lives = 6
+
+from hangman_art import logo
+print(logo)
+
+dispaly = []
+for
+
+
+
+
